ca6f97a2f58245ac76624448d18f11ebf66c8d0e
Sara <sarabaagar1@gmail.com>
2024-06-11 14:46:08 +0300

    Add models, views and some updates

--- lms/lms_app/views.py
+++ lms/lms_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request) :
-    return render(request, 'pages/index.html')
+    context = {
+        'category' : Category.objects.all(),
+        'books' : Book.objects.all(),
+    }
+    return render(request, 'pages/index.html', context)
 
 def books(request) :
-    return render(request, 'pages/books.html')
+    context = {
+        'category' : Category.objects.all(),
+        'books' : Book.objects.all(),
+    }
+    return render(request, 'pages/books.html', context)
